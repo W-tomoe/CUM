7f2fea639798963eaf5e1aea2ead0c94fa367632
wong <375640259@qq.com>
2019-05-09 01:06:18 +0800

    12

--- src/assets/js/process.js
+++ src/assets/js/process.js
@@ -1,18 +1,53 @@
 const proccess = {
     init() {
-        this.proccess =  $('.progress-track')
-        this.arrowLeft = $('.arrow-button--left')
-        this.arrowRight = $('.arrow-button--right')
-        this.albumStrip = $('.album-strip')
+        this.$proccess =  $('.progress-track')
+        this.$arrowLeft = $('.arrow-button--left')
+        this.$arrowRight = $('.arrow-button--right')
+        this.$albumStrip = $('.album-strip')
+
+        this.duration = 1
+
+        this.initalProY = this.$proccess.css('top') + this.$proccess.outerHeight(true)
+        this.initalArrow = this.$arrowLeft.outerHeight(true)
+        TweenMax.set(this.$proccess,{
+            y:-this.initalY,
+            ease:Quad.easeInOut
+        })
+
+        TweenMax.set([this.$arrowLeft, this.$arrowRight],{
+            y:this.initalArrow,
+            ease:Quad.easeInOut
+        })
+
+        TweenMax.set(this.$albumStrip,{
+            opacity: 0
+        })
     },
     show() {
+        TweenMax.set(this.$arrowRight,this.duration,{
+            y:0,
+            ease:Quad.easeInOut
+        })
 
+        TweenMax.set(this.$proccess,this.duration,{
+            y:0,
+            ease:Quad.easeInOut
+        })
+
+        TweenMax.set(this.$albumStrip,{
+            opacity: 1
+        })
     },
-
     leave() {
 
     },
-    click() {
+    clickProccess() {
+
+    },
+    clickArrowLeft() {
+
+    },
+    clickArrowRight() {
 
     },
     update() {
